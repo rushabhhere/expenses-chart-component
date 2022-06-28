fa44e68e94f162e42e3ab3f814650dade3efbc4e
Rushabh Javeri <javeri.rushabh45@gmail.com>
2022-06-28 23:52:41 +0530

    added comments to explain my logic

--- js/index.js
+++ js/index.js
@@ -7,18 +7,24 @@ const days = document.querySelectorAll('.day');
 const bars = document.querySelectorAll('.day .bar');
 const amounts = document.querySelectorAll('.day .amount');
 
+// determining today (to display bar in cyan)
 const date = new Date();
 const today = date.getDay() === 0 ? 6 : date.getDay() - 1;
 
+// determining how the bars will scale to fit within 155px
 const scale = scaleLinear()
-  .domain([0, max(data, d => d.amount)])
-  .range([0, 155]);
+  .domain([0, max(data, d => d.amount)]) // minimum possible amount and maximum amount spent
+  .range([0, 155]); // minimum and maximum bar size
 
 data.forEach((day, index) => {
+  // adding tooltip
   amounts[index].textContent = `$${day.amount}`;
+
+  // marking today
   if (index === today) {
     days[index].classList.add('today');
   }
 
+  // assigning heights to the bars
   bars[index].style.height = `${scale(day.amount)}px`;
 });
